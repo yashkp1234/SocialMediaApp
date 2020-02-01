e1230924fffa2c552d08cf72aabe5dedafdead50
yashkp1234 <yashkp123@gmail.com>
2020-02-01 15:30:14 -0500

    Added User Authentication

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config");
+const typeDefs = require("./graphql/typdefs");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port });
+  })
+  .then(res => {
+    console.log(`Server running at ${res.url}`);
+  });

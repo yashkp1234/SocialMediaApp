ad0feb93b2844d80df4022fe3619c5a2caf1d1a3
yashkp1234 <yashkp123@gmail.com>
2020-02-11 13:53:44 -0500

    added configs

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import { createUploadLink } from "apollo-upload-client";
 const port = process.env.PORT || 5000;
 
 const uploadLink = createUploadLink({
-  uri: "http://localhost:" + port
+  uri: "https://yellyash.herokuapp.com/" + port
 });
 
 const authLink = setContext(() => {
